420e60d1bdff0bf61aaadd53f9cd1cd355c277a1
MG <mg84@bk.ru>
2020-07-12 22:38:39 +0300

    set sending data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,85 @@
-import fp2 from 'fingerprintjs2'
+import Fingerprint2 from 'fingerprintjs2'
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log(223456)
-})
+const config = {
+  sendInterval: 10000,
+  apiUrl: 'http://localhost:3000/api/activities/add',
+}
+
+const cash = {
+  fingerprint: null,
+  activities: [],
+}
+
+const fpConfig = {
+  fonts: {extendedJsFonts: true},
+}
+
+function getFingerprint() {
+  return new Promise((done) => {
+    if (window.requestIdleCallback) {
+      requestIdleCallback(() => {
+        Fingerprint2.getV18(fpConfig, (hash, components) => {
+          done({hash, components})
+        })
+      })
+    } else {
+      setTimeout(() => {
+        Fingerprint2.getV18(fpConfig, (hash, components) => {
+          done({hash, components})
+        })
+      }, 500)
+    }
+  })
+}
+
+function sender() {
+  if (!cash.activities.length) {
+    return
+  }
+
+  fetch(config.apiUrl, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(cash),
+  })
+    .catch((err) => console.log(err))
+    .finally(() => {
+      console.dir('send cash', cash)
+      cash.activities = []
+    })
+}
+
+function startSender() {
+  setInterval(sender, config.sendInterval)
+}
+
+async function start() {
+  const fingerprint = await getFingerprint()
+  console.log(fingerprint)
+
+  if (!fingerprint || !fingerprint.hash) {
+    return
+  }
+  cash.fingerprint = fingerprint.hash
+
+  startSender()
+
+  document.addEventListener('click', ((event) => {
+    const data = {
+      clientX: event.clientX,
+      clientY: event.clientY,
+      screenWidth: window.screen.width,
+      rotation: window.screen.orientation.type,
+      scrollX: window.scrollX,
+      scrollY: window.scrollY,
+      targetElemTag: event.target.nodeName,
+    }
+
+    cash.activities.push(data)
+    console.log(data)
+  }))
+}
+
+document.addEventListener('DOMContentLoaded', start)
